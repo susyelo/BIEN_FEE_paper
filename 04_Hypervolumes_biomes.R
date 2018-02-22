94409c63fe797d4ccd8906214c22438db1b92f34
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-02-22 17:00:36 -0500

    Trying to extract the Olson biome classification for each species suing a raster

--- 04_Hypervolumes_biomes.R
+++ 04_Hypervolumes_biomes.R
@@ -1,20 +1,31 @@
-
 # libraries ---------------------------------------------------------------
 library(hypervolumes)
-
+library(raster)
+library(rworldmap)
 
 # data --------------------------------------------------------------------
+# Olson shapefiles and raster files
 load("./data/Danilo_data/biomes_shp4susy.RData")
-biomes_grid<-read.table("./data/Danilo_data/biomes.csv")
-
-load("./outputs/BIEN_traits_PresAbs1degree.RData")
+load("./data/Olson_raster.RData")
 
 occ_data<-readRDS("./data/2018_02_08_BIEN_traitOccurData.RData")
 
 occ_data_geo<-occ_data
-
-
 coordinates(occ_data_geo)<-cbind(occ_data_geo$longitude , occ_data_geo$latitude)
 
+biomes_names<-levels(r_biomes)[[1]]
 
-tmp<-extract(occ_data_geo,dry@data)
+### Changing projections
+
+proj4string(occ_data_geo)<-proj4string(r_biomes)
+
+crs(occ_data_geo)<-crs(r_biomes)
+occ_data_geo_trnsfrmd = spTransform(occ_data_geo,crs(r_biomes))
+
+
+occ_data_geo$biomes<-extract(r_biomes,occ_data_geo,factors=TRUE)
+
+match(occ_data_geo$biomes, biomes_names$ID)
+
+plot(wrld_simpl,xlim=c(-179,-40),ylim=c(-20,80))
+points(occ_data_geo_trnsfrmd[is.na(occ_data_geo_trnsfrmd$biomes),],col="red",pch=20)
