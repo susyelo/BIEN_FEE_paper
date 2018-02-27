afabb172e495a0aceb6c9e169142c842d7424f0e
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-02-27 16:56:03 -0500

    Delete code that was used to explore the occurrence data. From now on I am using the range maps data

--- 04_Hypervolumes_biomes.R
+++ 04_Hypervolumes_biomes.R
@@ -5,27 +5,11 @@ library(rworldmap)
 
 # data --------------------------------------------------------------------
 # Olson shapefiles and raster files
-load("./data/Danilo_data/biomes_shp4susy.RData")
-load("./data/Olson_raster.RData")
+load("./data/base/Danilo_data/biomes_shp4susy.RData")
+load("./data/base/Olson_raster.RData")
 
-occ_data<-readRDS("./data/2018_02_08_BIEN_traitOccurData.RData")
+## Presence/absence matrix of species with traits
+load("./data/processed/PresAbs_matrix_TraitRange.RData")
 
-occ_data_geo<-occ_data
-coordinates(occ_data_geo)<-cbind(occ_data_geo$longitude , occ_data_geo$latitude)
-
-biomes_names<-levels(r_biomes)[[1]]
-
-### Changing projections
-
-proj4string(occ_data_geo)<-proj4string(r_biomes)
-
-crs(occ_data_geo)<-crs(r_biomes)
-occ_data_geo_trnsfrmd = spTransform(occ_data_geo,crs(r_biomes))
-
-
-occ_data_geo$biomes<-extract(r_biomes,occ_data_geo,factors=TRUE)
-
-match(occ_data_geo$biomes, biomes_names$ID)
-
-plot(wrld_simpl,xlim=c(-179,-40),ylim=c(-20,80))
-points(occ_data_geo_trnsfrmd[is.na(occ_data_geo_trnsfrmd$biomes),],col="red",pch=20)
+#Richness raster of the species with traits
+richness_ras<-raster("./data/processed/TraitRichness_raster.tif")
