cb0b473ed991ed26f131e3ef77a7450bf9545194
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-03-01 17:12:59 -0500

    Extract total richness per biomes and richness of species with traits per biome

--- 03a_Prepare_Spatial_Data_MapRanges.R
+++ 03a_Prepare_Spatial_Data_MapRanges.R
@@ -3,6 +3,8 @@ library(letsR)
 library(raster)
 library(gridExtra)
 library(tidyverse)
+library(foreach)
+library(maptools)
 
 
 # Functions ---------------------------------------------------------------
@@ -20,6 +22,9 @@ names(d) = c("Species","Y","X")
 # Richness raster
 r<-raster("./data/base/BIEN_2_Ranges/richness100km.tif")
 
+# Biomes polygons
+load("./data/base/Danilo_data/biomes_shp4susy.RData")
+
 
 # Total richness ----------------------------------------------------------
 ### Total richness for species with ranges maps in BIEN 2.0
@@ -33,14 +38,12 @@ Total_richness_plot<-spplot(r, main=paste("Total richness:",n_distinct(d$Species
 # process trait dataframe
 trait_df$scrubbed_species_binomial<-gsub(" ","_",trait_df$scrubbed_species_binomial)
 
-trait_df_wide<-
+trait_df_range<-
   trait_df  %>% 
-  spread(trait_name,trait_value) %>% 
   filter(scrubbed_species_binomial%in%d$Species)
 
-
 # matrix and richness raster
-TraitSpecies = unique(trait_df_wide$scrubbed_species_binomial)
+TraitSpecies = unique(trait_df_range$scrubbed_species_binomial)
 
 ## It include NAs for the species that do not have range maps available
 spMatrix = splistToMatrix(d,TraitSpecies)
@@ -54,7 +57,7 @@ Trait_richness_plot<-spplot(spRichness, main=paste("Species with traits",length(
 
 ## Richness for woody vs herbaceous species
 ## Woody species
-woody_sp<-trait_df_wide %>% 
+woody_sp<-trait_df_range %>% 
   filter(GROWTHFORM_GEN=="woody") %>% 
   dplyr::select(scrubbed_species_binomial) %>% 
   unique
@@ -65,7 +68,7 @@ Woody_plot<-spplot(Woody_Richness, main=paste("Woody",n_distinct(woody_sp$scrubb
 
 
 ## Herbaceous species
-herbaceous_sp<-trait_df_wide %>% 
+herbaceous_sp<-trait_df_range %>% 
   filter(GROWTHFORM_GEN=="herbaceous") %>% 
   dplyr::select(scrubbed_species_binomial) %>% 
   unique
@@ -87,4 +90,67 @@ dev.off()
 writeRaster(spRichness,"./data/processed/TraitRichness_raster.tif")
 save(spMatrix, file="./data/processed/PresAbs_matrix_TraitRange.RData")
 
+
+## Species richness per biome
+
+biome_poly<-list(coniferous,dry,grasslands,
+                 moist,montane,prairies,savanna,taiga,
+                 temperate.mixed,tropical.mixed,tundra)
+
+names(biome_poly)<-c("coniferous","dry","grasslands",
+                     "moist","montane","prairies","savanna","taiga",
+                     "temperate.mixed","tropical.mixed","tundra")
+
+biome_richness<-foreach(i=1:length(biome_poly))  %do% {
+  
+  unlist(raster::extract(r,biome_poly[[i]]))
+
+}
+
+names(biome_richness)<-names(biome_poly)
+
+
+## transform data
+require(reshape2)
+biome_richness_df <- melt(biome_richness)
+colnames(biome_richness_df)<-c("N_Species","Biome")
+biome_richness_df<-na.omit(biome_richness_df)
+
+## Plotting Violin plots for each biome
+p <- ggplot(biome_richness_df, 
+            aes(x=reorder(Biome, N_Species, FUN=mean), y=N_Species)) +
+  geom_violin(trim=TRUE)
+
+p + stat_summary(fun.data=mean_sdl, 
+                 geom="pointrange", color="red") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  xlab("Biomes") + ylab("Richness")
+
+
+### Species richness with traits per biome ----
+
+Biome_richness_trait<-foreach(i=1:length(biome_poly))  %do% {
+  
+  unlist(raster::extract(spRichness,biome_poly[[i]]))
+  
+}
+
+names(Biome_richness_trait)<-names(biome_poly)
+
+
+## transform data
+require(reshape2)
+biome_richness_trait_df <- melt(Biome_richness_trait)
+colnames(biome_richness_trait_df)<-c("N_Species","Biome")
+biome_richness_trait_df<-na.omit(biome_richness_trait_df)
+
+## Plotting Violin plots for each biome
+p <- ggplot(biome_richness_trait_df, 
+            aes(x=reorder(Biome, N_Species, FUN=mean), y=N_Species)) +
+  geom_violin(trim=TRUE)
+
+p + stat_summary(fun.data=mean_sdl, 
+                 geom="pointrange", color="red") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  xlab("Biomes") + ylab("Richness")
 
