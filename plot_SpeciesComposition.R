f25233d0d0883f8f455553c5e009c108f4b7270c
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-03-05 11:07:45 -0500

    * Include cell number into the presence matrix * Extract species list per biomes using the cell number as reference

--- plot_SpeciesComposition.R
+++ plot_SpeciesComposition.R
@@ -11,7 +11,8 @@ source("./functions/BIEN2.0_RangeMaps_functions.R")
 ## I am still not sure the order of the variables in the table
 # TODO: Check this! 
 spPresence<-read.csv("./data/base/BIEN_2_Ranges/presence100km.csv",header = FALSE,
-                     col.names = c("Species","X","Y"))
+                     col.names = c("Species","Y","X"))
+
 
 #2. Total_richness raster
 r_Total_Rich<-raster("./data/base/BIEN_2_Ranges/richness100km.tif")
@@ -22,7 +23,11 @@ r_Total_Rich[r_Total_Rich==0]<-NA
 #spplot(r_Total_Rich)
 
 # 3. Shapefiles
-load("./data/Danilo_data/biomes_shp4susy.RData")
+load("./data/base/Danilo_data/biomes_shp4susy.RData")
+
+## Include cell number using the row and col numbers as reference
+spPresence$cells<-cellFromRowCol(r_Total_Rich,spPresence$Y, spPresence$X)
+
 
 # Extract species for each biome --------------------------------------------
 # Ignoring montane TODO: do it with better polygons
@@ -36,23 +41,28 @@ names(biome_poly)<-c("coniferous","dry","grasslands",
 
 biome_richness<-foreach(i=1:length(biome_poly))  %do% {
   
-  cells_tmp<-unlist(cellFromPolygon(r_Total_Rich,coniferous))
-  sp_list_tmp<-spFromCell(d=spPresence,cell = cells_tmp, r=r_Total_Rich)
+  cells_tmp<-unlist(cellFromPolygon(r_Total_Rich,biome_poly[[i]]))
+  sp_list_tmp<-unique(spPresence$Species[spPresence$cells%in%cells_tmp])
   
 }
-
 names(biome_richness)<-names(biome_poly)
 
 
 ## Create similarity matrix
+## Create a loop to calculate the similarity (number of species shared among biomes)
 
-# Make matrices to store the final results, temporary sums and counts
-count.matrix<-matrix(0,nrow=length(biome_richness),ncol=length(biome_richness))
+spSimilarity<-foreach(i=1:length(biome_richness), .combine='cbind') %:%
+  foreach(j=1:length(biome_richness), .combine='c') %do% {
+    length(intersect(biome_richness[[i]],biome_richness[[j]]))
+  }
 
-## Create a loop to calculate the similarity
-length(intersect(biome_richness$coniferous,biome_richness$dry))
+colnames(spSimilarity)<-names(biome_richness)
+rownames(spSimilarity)<-names(biome_richness)
+
+## Double check that the numbers are correct
+#diag(spSimilarity)==unlist(lapply(biome_richness, n_distinct))
 
 
-  
+# Chordplot of similarities -----------------------------------------------
 
 
