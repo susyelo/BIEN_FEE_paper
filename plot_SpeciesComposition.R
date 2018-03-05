4b127aa53972a416626f1273b0a83af817122d53
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-03-05 12:33:16 -0500

    Plot number of species shared among biomes using a chord plot (with all the species)

--- plot_SpeciesComposition.R
+++ plot_SpeciesComposition.R
@@ -31,13 +31,15 @@ spPresence$cells<-cellFromRowCol(r_Total_Rich,spPresence$Y, spPresence$X)
 
 # Extract species for each biome --------------------------------------------
 # Ignoring montane TODO: do it with better polygons
-biome_poly<-list(coniferous,dry,grasslands,
-                 moist,prairies,savanna,taiga,
-                 temperate.mixed,tropical.mixed,tundra)
-
-names(biome_poly)<-c("coniferous","dry","grasslands",
-                     "moist","prairies","savanna","taiga",
-                     "temperate.mixed","tropical.mixed","tundra")
+biome_poly<-list(moist,tropical.mixed,
+                 savanna,grasslands,
+                 dry,xeric,mediterranean,
+                 temperate.mixed,coniferous,prairies,taiga,tundra)
+  
+names(biome_poly)<-c("moist","tropical.mixed",
+                     "savanna","grasslands",
+                     "dry","xeric","mediterranean",
+                     "temperate.mixed","coniferous","prairies","taiga","tundra")
 
 biome_richness<-foreach(i=1:length(biome_poly))  %do% {
   
@@ -63,6 +65,40 @@ rownames(spSimilarity)<-names(biome_richness)
 #diag(spSimilarity)==unlist(lapply(biome_richness, n_distinct))
 
 
+spSimilarity/diag(spSimilarity)
 # Chordplot of similarities -----------------------------------------------
+
+library(circlize)
+library(RColorBrewer)
+library(wesanderson)
+
+col=c(wes_palette("Darjeeling",6,type="continuous"),
+      wes_palette("Cavalcanti",6,type="continuous"))
+
+
+adj = matrix(sample(c(1, 0), 26**2, replace = TRUE, prob = c(1, 9)),
+             nrow = 26, dimnames = list(LETTERS, LETTERS))
+adj = ifelse(adj == 1, runif(26**2), 0)
+chordDiagram(adj, transparency = 0.4, grid.col = "midnightblue",
+             col = colorRamp2(seq(0, 1, 0.2), brewer.pal(6, "Blues")))
+
+spSimilarity_1<-spSimilarity
+diag(spSimilarity_1)<-0
+chordDiagram(spSimilarity_1, transparency = 0.25,
+             grid.col =col,link.sort = TRUE,symmetric = TRUE)
+
+
+colnames(spSimilarity_1)<-c("moist","tropical.mixed",
+                            "savanna","grasslands",
+                            "dry","xeric","MED",
+                            "TEMP","CONI","PRA","TA","TU")
+
+rownames(spSimilarity_1)<-colnames(spSimilarity_1)
+
+pdf("./figs/Total_similarity_biomes.pdf")
+chordDiagram(spSimilarity_1, grid.col =col,symmetric = TRUE,link.sort = TRUE,
+             column.col = col)
+dev.off()
+
 
 
