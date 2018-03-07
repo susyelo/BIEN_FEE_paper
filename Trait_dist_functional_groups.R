93ca2763b5a1e705793f7698a2d4b27b29943a66
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-03-07 18:00:44 -0500

    Code to calculate distance matrices among species according to their traits

---
+++ Trait_dist_functional_groups.R
@@ -0,0 +1,19 @@
+# library -----------------------------------------------------------------
+library(funrar)
+library(tidyverse)
+
+
+# data --------------------------------------------------------------------
+DF_trait_biome<-read.csv("./outputs/Df_traits_biomes_Sp_withRanges.csv")
+
+
+DF_trait_biome_sub<-
+  DF_trait_biome%>%
+  select(scrubbed_species_binomial,SLA,Seed_mass,Height,Leaf_P,Leaf_N,Wood_density) %>% 
+  distinct
+
+rownames(DF_trait_biome_sub)<-DF_trait_biome_sub$scrubbed_species_binomial
+
+# Compute distance matrix of trait between each pair of species 
+Dist_matrix<-compute_dist_matrix(DF_trait_biome_sub[,-1],metric="euclidean",center = TRUE,
+                                 scale = TRUE) ## This can take a while
