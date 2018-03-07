1a49f42ccf186e5111a422d75337f6aa7e702158
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-03-07 18:09:14 -0500

    Include extra notes to calculate functional distinctiveness among biomes

--- Trait_dist_functional_groups.R
+++ Trait_dist_functional_groups.R
@@ -17,3 +17,7 @@ rownames(DF_trait_biome_sub)<-DF_trait_biome_sub$scrubbed_species_binomial
 # Compute distance matrix of trait between each pair of species 
 Dist_matrix<-compute_dist_matrix(DF_trait_biome_sub[,-1],metric="euclidean",center = TRUE,
                                  scale = TRUE) ## This can take a while
+
+
+## Check this file for further analysis
+# /Users/echeverrialondono1/Git_Repos/BIEN_project/Functional_Div/4__Dominants_Subordinates_Dist.R
