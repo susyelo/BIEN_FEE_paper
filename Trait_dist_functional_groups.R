9c9a2868877955b2397bd46db353f621505a230a
Susy Echeverria Londoño <susy.echeverria-londono12@imperial.ac.uk>
2018-03-09 17:51:40 -0500

    Create plot of growth forms proportions dividing species between Distinctive and redundant

--- Trait_dist_functional_groups.R
+++ Trait_dist_functional_groups.R
@@ -4,11 +4,20 @@ library(tidyverse)
 library(sp)
 library(foreach)
 library(raster)
+library(viridis)
+
 
 
 # Functions ---------------------------------------------------------------
 source("./functions/funrar_functions_mod.R")
 source("./functions/check_functions.R")
+
+rescaleRas <- function(x, x.min = NULL, x.max = NULL, new.min = 0, new.max = 1) {
+  if(is.null(x.min)) x.min = min(x,na.rm=TRUE)
+  if(is.null(x.max)) x.max = max(x,na.rm=TRUE)
+  new.min + (x - x.min) * ((new.max - new.min) / (x.max - x.min))
+}
+
 
 # data --------------------------------------------------------------------
 # 1. Trait data frame
@@ -94,9 +103,100 @@ Biome_relAbun<-make_relative(Biomes_Abun_sp)
 
 # Calculating distinctiveness using relative abundance (number of grids) of species ----
 Biomes_di = distinctiveness_mod(Biome_relAbun, Dist_matrix)
-Biomes_di_tmp = distinctiveness(Biome_relAbun, Dist_matrix)
 
-tmp<-colSums(Biomes_di,na.rm = TRUE)
-length(which(tmp>0))
-## Check this file for further analysis
-# /Users/echeverrialondono1/Git_Repos/BIEN_project/Functional_Div/4__Dominants_Subordinates_Dist.R
+Biomes_di_clean<-
+  Biomes_di%>%
+  as.matrix %>% 
+  t()%>%
+  as.data.frame()%>%
+  mutate(species=colnames(Biomes_di))%>%
+  gather(key="Biome",value="Di",-species) %>% 
+  filter(!is.na(Di))
+
+## Get rid of some outliers
+unique(Biomes_di_clean$species[which(Biomes_di_clean$Di>10)])
+
+Biomes_di_clean<-
+  Biomes_di_clean %>% 
+  filter(Di<10)
+  
+
+
+### Exploring data
+Biomes_di_clean %>% 
+  group_by(Biome) %>% 
+  summarise(mean_di=mean(Di),
+            sd_di=sd(Di))
+
+summary(Biomes_di_clean$Di[which(Biomes_di_clean$Biome=="Moist_Forest")])
+Biomes_di_clean$Discale<-rescaleRas(Biomes_di_clean$Di)
+
+### Merging growth form data with distinctiveness data
+
+df1<-
+  DF_trait_biome %>% 
+  dplyr::select(scrubbed_species_binomial,GROWTHFORM_GEN,GROWTHFORM_STD) %>% 
+  distinct
+
+Biome_di_growthForm<-merge(df1,Biomes_di_clean,
+                           by.x="scrubbed_species_binomial",
+                           by.y="species")
+
+biome_name<-unique(Biome_di_growthForm$Biome)
+
+dist<-
+foreach(i=1:length(biome_name), .combine = rbind) %do%{
+  a<-Biome_di_growthForm %>% 
+    filter(Biome==biome_name[i]) %>% 
+    filter(Discale>=quantile(Discale, probs =0.6)) %>% 
+    group_by(GROWTHFORM_STD) %>% 
+    summarise(N_sp=length(GROWTHFORM_STD)) %>% 
+    mutate(Dist="Dist",Biome=biome_name[i],prop=round(N_sp/sum(N_sp)*100,2))
+}
+
+redunt<-
+  
+  foreach(i=1:length(biome_name), .combine = rbind) %do%{
+    a<-Biome_di_growthForm %>% 
+      filter(Biome==biome_name[i]) %>% 
+      filter(Discale<quantile(Discale, probs =0.2)) %>% 
+      group_by(GROWTHFORM_STD) %>% 
+      summarise(N_sp=length(GROWTHFORM_STD)) %>% 
+      mutate(Dist="Redun",Biome=biome_name[i],prop=round(N_sp/sum(N_sp)*100,2))
+  }
+
+dist<-
+dist %>% 
+  filter(GROWTHFORM_STD!="Aquatic")
+
+redunt<-
+  redunt %>% 
+  filter(GROWTHFORM_STD!="Aquatic")
+
+
+
+total_dist<-rbind(dist,redunt)
+
+pdf("./figs/Growth_distinctive.pdf", width = 12)
+ggplot() + geom_bar(aes(y = prop, x = Biome, fill = GROWTHFORM_STD), data = dist,
+                    stat="identity")+
+  coord_flip() +
+  ggtitle("Distinctive species")
+dev.off()
+
+
+pdf("./figs/Growth_redundant.pdf", width = 12)
+ggplot() + geom_bar(aes(y = prop, x = Biome, fill = GROWTHFORM_STD), data = redunt,
+                          stat="identity")+
+  coord_flip() +
+  ggtitle("Redundant species")
+dev.off()  
+
+
+ggplot(redunt) +      
+  # Add the stacked bar
+  geom_bar(aes(x=as.factor(Biome), y=prop, fill=GROWTHFORM_STD),stat="identity") +
+  coord_flip() 
+
+
+
