759dbac6f3a354aaf31a7c732a0ff1bb1f286265
susyelo <susyelo@gmail.com>
2018-03-25 18:29:08 -0400

    Include functions to make the code more effient and easier to read.

--- 05b_Trait_dist_functional_groups_phylo.R
+++ 05b_Trait_dist_functional_groups_phylo.R
@@ -7,19 +7,13 @@ library(raster)
 library(viridis)
 library(ggridges)
 library(wesanderson)
+library(lattice)
 
 
 # Functions ---------------------------------------------------------------
-source("./functions/funrar_functions_mod.R")
 source("./functions/check_functions.R")
 source("./functions/BIEN2.0_RangeMaps_functions.R")
-
-rescaleRas <- function(x, x.min = NULL, x.max = NULL, new.min = 0, new.max = 1) {
-  if(is.null(x.min)) x.min = min(x,na.rm=TRUE)
-  if(is.null(x.max)) x.max = max(x,na.rm=TRUE)
-  new.min + (x - x.min) * ((new.max - new.min) / (x.max - x.min))
-}
-
+source("./functions/trait_distribution_functions.R")
 
 # data --------------------------------------------------------------------
 # 1. Trait data frame
@@ -42,70 +36,35 @@ Growth_form$SPECIES_STD<-gsub(" ","_",Growth_form$SPECIES_STD)
 # Check raster and shapefiles are in the same projection
 #proj4string(r_Total_Rich)==proj4string(biome_shp)
 
-# Presence absence matrix -------------------------------------------------
+# Presence absence matrix of species in each biome -------------------------------------------------
 TraitSpecies <- unique(Traits_phylo$species)
 spMatrix_sub <- splistToMatrix(spPresence,TraitSpecies)
 
-# Extract cells per biome
-## Include biome classification in each cell 
-biome_name<-biome_shp$biomes
-
-cells_biomes<-data.frame(cells=rownames(spMatrix_sub))
-cells_biomes$biomes<-NA
-
-for (i in 1:length(biome_name)){
-  print(paste("Extracting",biome_name[i]))
-  shp_tmp<-biome_shp[which(biome_shp$biomes==biome_name[i]),]
-  cells_tmp<-unlist(cellFromPolygon(r_Total_Rich,shp_tmp))
-  cells_tmp<-paste("Cell",cells_tmp,sep="_")
-  cells_biomes$biomes[cells_biomes$cells%in%cells_tmp]<-biome_name[i]
-}
-
-cells_biomes<-na.omit(cells_biomes)
-
-## Biome presence and absence matrix
-biome_PAbs_matrix<-spMatrix_sub
-indx<-match(rownames(spMatrix_sub),cells_biomes$cells)
-rownames(biome_PAbs_matrix)<-cells_biomes$biomes[indx]
-
-# Filter cells without information
-biome_PAbs_matrix<-biome_PAbs_matrix[!is.na(rownames(biome_PAbs_matrix)),]
-rm(spMatrix_sub)
+Biomes_pabs<-spMatrix_biomes(spMatrix = spMatrix_sub,
+                             biome_shp = biome_shp,
+                             raster_ref = r_Total_Rich)
 
 # Compute distance matrix of trait between each pair of species  ----------
 rownames(Traits_phylo)<-Traits_phylo$species
-Dist_matrix<-compute_dist_matrix(Traits_phylo[,-7],metric="euclidean",center = TRUE,
+
+# Select traits to calculate the distances among species
+traits<-c("Wood_density","Leaf_N","SLA","Seed_mass","Height","Leaf_P")
+
+
+traits<-c("Wood_density","Seed_mass")
+
+Dist_matrix<-compute_dist_matrix(Traits_phylo[,traits],metric="euclidean",
+                                 center = TRUE,
                                  scale = TRUE) ## This can take a while
 
 
 
 # Compute functional distinctiveness per biome ----------------------------
 
-# Matrix with number of times the species is present in a grid per biome
-# This could be use as relative abundance of species in each of the biomes
-biomes_names<-unique(row.names(biome_PAbs_matrix))
-
-Biomes_Abun_sp<-foreach(i=1:length(biomes_names),.combine=rbind)%do%
-{
-  
-  indx<-which(row.names(biome_PAbs_matrix)==biomes_names[i])
-  biome_abun<-colSums(biome_PAbs_matrix[indx,])
-  
-}
-row.names(Biomes_Abun_sp)<-biomes_names
-
 # Calculating relative abundance
-Biome_relAbun<-make_relative(Biomes_Abun_sp)
+Biome_relAbun<-make_relative(Biomes_pabs$Biomes_Abun)
 
-# Calculating distinctiveness ----
-Biomes_pabs_sp<-Biomes_Abun_sp
-Biomes_pabs_sp[which(Biomes_pabs_sp>0)]<-1
-
-# Number of species that do not fall in any of the biomes
-# tmp<-colMeans(Biomes_pabs_sp)
-# length(which(tmp==0)) ## There are around 80 species
-
-Biomes_di = distinctiveness(Biomes_pabs_sp, Dist_matrix)
+Biomes_di = distinctiveness(Biome_relAbun, Dist_matrix)
 
 Biomes_di_clean<-
   Biomes_di%>%
@@ -117,32 +76,36 @@ Biomes_di_clean<-
   filter(!is.na(Di))
 
 
-## Calculate restrictness
+biome_names=biome_shp$biomes
+
+# Compute functional restrictiness per biome ----------------------------
 ## A 0 value indicates that the focal species is present in all the sites.
 rest_species<-
-  foreach(i=1:length(biomes_names), .combine = rbind)%do%
+  foreach(i=1:length(biome_names), .combine = rbind)%do%
   {
-    indx<-which(rownames(biome_PAbs_matrix)==biome_name[i])
-    biome_PAbs_tmp<-biome_PAbs_matrix[indx,]
+    indx<-which(rownames(Biomes_pabs$Biomes_pabs_cells)==biome_name[i])
+    biome_PAbs_tmp<-Biomes_pabs$Biomes_pabs_cells[indx,]
     rest_species<-restrictedness(pres_matrix = biome_PAbs_tmp)
     rest_species$Biome<-biome_name[i]
-   
+    
     rest_species
   }
 
 ## Exclude all that are not present in any biome cell (which are not in Biome_Di)
+# Usually species restricted to just one cell which is not classified in any of the biomes
 rest_species<-
   rest_species %>% 
   filter(species%in%unique(Biomes_di_clean$species))
 
-## Merge Di and Ri
-Biome_Di_Ri<-merge(Biomes_di_clean, rest_species)
 
 ## Scaling Di values per biome
 Biomes_di_clean<-
   Biomes_di_clean %>% 
   group_by(Biome) %>% 
-  mutate(DiScale=rescaleRas(Di))
+  mutate(DiScale=rescaleRas01(Di))
+
+## Merge Di and Ri
+Biome_Di_Ri<-merge(Biomes_di_clean, rest_species)
 
 Biome_Di_Ri$FunDi<-(Biome_Di_Ri$DiScale+Biome_Di_Ri$Ri)/2
 Biome_Di_Ri$FunDi2<-Biome_Di_Ri$Di*Biome_Di_Ri$Ri
@@ -153,7 +116,7 @@ write.csv(Biome_Di_Ri, "./outputs/Biome_Di_Ri_phylo.csv")
 ## Heatmaps
 # Total headmap
 
-Biome_Di_Ri$bin_Di<-cut(Biome_Di_Ri$Di, breaks = 10)
+Biome_Di_Ri$bin_Di<-cut(Biome_Di_Ri$DiScale, breaks = 10)
 Biome_Di_Ri$bin_Ri<-cut(Biome_Di_Ri$Ri, breaks = 10)
 
 get_matrix<-function(df=Biome_Di_Ri, Biome_name="Total")
---
+++ functions/trait_distribution_functions.R
@@ -0,0 +1,66 @@
+
+
+rescaleRas01 <- function(x, x.min = NULL, x.max = NULL, new.min = 0, new.max = 1) {
+  if(is.null(x.min)) x.min = min(x,na.rm=TRUE)
+  if(is.null(x.max)) x.max = max(x,na.rm=TRUE)
+  new.min + (x - x.min) * ((new.max - new.min) / (x.max - x.min))
+}
+
+
+fromCell_to_Biomes<-function(biome_shp, raster_ref)
+{
+  require(foreach)
+  # Extract the names of the biomes from the shapefile
+  biomes_names= biome_shp$biomes
+  
+  foreach(i=1:length(biomes_names), .combine = rbind)%do%{
+    
+    print(paste("Extracting",biomes_names[i]))
+    shp_tmp<-biome_shp[which(biome_shp$biomes==biomes_names[i]),]
+    cells_tmp<-unlist(cellFromPolygon(raster_ref,shp_tmp))
+    cells_tmp<-paste("Cell",cells_tmp,sep="_")
+    df_cell_biomes<-data.frame(cells=cells_tmp,biomes=biomes_names[i])
+    
+  }
+  
+}
+
+
+spMatrix_biomes<-function(spMatrix, biome_shp, raster_ref){
+  
+  # spMatrix= Species presence absence matrix
+  # biomes shapefile
+  # Reference raster for projections and position of cells
+  cells_biomes<-fromCell_to_Biomes(biome_shp = biome_shp, 
+                                   raster_ref = raster_ref)
+  
+  
+  ## Species presence and absence in biomes matrix
+  biome_PAbs_matrix<-spMatrix
+  indx<-match(rownames(spMatrix),cells_biomes$cells)
+  rownames(biome_PAbs_matrix)<-cells_biomes$biomes[indx]
+  biome_PAbs_matrix<-biome_PAbs_matrix[!is.na(rownames(biome_PAbs_matrix)),] # Remove cells without species
+  
+  
+  # 1. Matrix with number of times the species is present in a grid per biome
+  biomes_names<-unique(row.names(biome_PAbs_matrix))
+
+  Biomes_Abun_sp<-foreach(i=1:length(biomes_names),.combine=rbind)%do%
+  {
+    
+    indx<-which(row.names(biome_PAbs_matrix)==biomes_names[i])
+    biome_abun<-colSums(biome_PAbs_matrix[indx,])
+    
+  }
+  row.names(Biomes_Abun_sp)<-biomes_names
+  
+  #2. Final presence/absence matrix species vs biomes
+  Biomes_pabs_sp<-Biomes_Abun_sp
+  Biomes_pabs_sp[which(Biomes_pabs_sp>0)]<-1
+  
+  
+  Biomes_pabs_final<-list(Biomes_Abun=Biomes_Abun_sp, 
+                          Biomes_pabs=Biomes_pabs_sp, 
+                          Biomes_pabs_cells=biome_PAbs_matrix)
+}
+
