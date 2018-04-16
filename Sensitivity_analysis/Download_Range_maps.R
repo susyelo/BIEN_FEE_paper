ea5cf60b1543280d82cdbd4fa62e125414df83e3
susyelo <susyelo@gmail.com>
2018-04-15 22:40:39 -0400

    Include for loop to extract the species ranges

--- Sensitivity_analysis/Download_Range_maps.R
+++ Sensitivity_analysis/Download_Range_maps.R
@@ -4,6 +4,7 @@ library(ape)
 library(picante)
 library(tidyverse)
 library(raster)
+library(foreach)
 
 
 # Data --------------------------------------------------------------------
@@ -27,25 +28,40 @@ Trait_BIEN_df %>%
 
 # Download range maps -----------------------------------------------------
 range_maps<-BIEN_ranges_load_species(Trait_BIEN_df$scrubbed_species_binomial)
-saveRDS(range_maps, "./data/processed/BIEN3.0SeedPhylo_maps.rds")
+saveRDS(range_maps, "./data/base/BIEN3.0SeedPhylo_maps.rds")
 
-range_maps_df<-spTransform(range_maps, CRS("+proj=laea +lat_0=15 +lon_0=-80 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs
-+ellps=WGS84 +towgs84=0,0,0")) 
 
+
+## Create baseline raster: to extract the presence/absence of species in each cell
+# Base for resolution and projection
 resol<-2
-r<-raster(xmn = -167, xmx = -29,ymn = -57, ymx = 83,
-          crs = CRS("+init=epsg:4326 +proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-res(r) <- resol
+r_base<-raster(xmn = -167, xmx = -29,ymn = -57, ymx = 83,
+               crs = CRS(proj4string(r)))
 
-p <- rasterToPolygons(r)
-p$cell <- 1:ncell(r)
+res(r_base) <- resol
+values(r_base)<-1:ncell(r_base)
+r_base<-projectRaster(r_base, r)
+
+p <- rasterToPolygons(r_base)
+p$cell <- 1:ncell(r_base)
 p$layer <- NULL
 
-v <- intersect(range_maps, p)
+# Absence/presence matrix of species
+num<-seq(1,length(range_maps),100)
+tmp_num<-data.frame(n1=num, n2=c(num[-1]+1,length(range_maps)))
 
-m <- as.matrix(table(v$cell, v$species))
-m[1:10, ]
-
-
-range_maps_ras<- rasterize(range_maps, r)
+Cells_sp<-
+foreach(i=1:nrow(tmp_num), .combine = rbind)%do%{
+  
+  col1<-tmp_num$n1[i]
+  col2<-tmp_num$n2[i]
+  print(paste("Extract ranges from",range_maps$species[col1], "to", range_maps$species[col2]))
+  range_maps_sp<-range_maps[c(col1:col2),]
+  range_maps_df<-spTransform(range_maps_sp, CRS(proj4string(r)))
+  
+  v <- over(range_maps_df,p)
+  v$species<-range_maps_df$species
+  
+  v
+}
 
